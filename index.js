55ac33bdb6c9bad7059cdd6b8918d0073b28aab3
michela iacobucci <michelaiacobucci@MacBook-Air.local>
2020-07-02 14:19:09 -0700

    stage1

---
+++ index.js
@@ -0,0 +1,23 @@
+// Import libraries 
+
+// ROUTES 
+
+// homepage - GET main index of the site/API
+// homepage - POST and show in slideshow
+
+// register get route 
+// register post route 
+
+// login get route 
+// login post route 
+
+// profile get route
+// profile post route 
+// users - GET details of one user 
+// GET page with favorited songs/playlists 
+// POST - receive name of a song and add it to the database 
+// DESTROY - delete a song by it's ID 
+
+
+// logout get route 
+// logout post route 
